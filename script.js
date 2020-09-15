e14e27ddf1a72bb3cefa0ad372fdef7c6ceb4e4a
buchacha <egor322129@gmail.com>
2020-09-15 09:03:46 +0000

    filter with custom callback

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-function makeArmy() {
-  let shooters = [];
-
-
-  for (let i = 0; i < 10; i++) {
-    let shooter = function() { // функция shooter
-      alert( i ); // должна выводить порядковый номер
-    };
-    shooters.push(shooter);
-  }
-
-  return shooters;
+function inArray(arr) {
+  return function(x) {
+    return arr.includes(x);
+  };
 }
 
-let army = makeArmy();
-
-army[0](); // у 0-го стрелка будет номер 10
-army[5](); // и у 5-го стрелка тоже будет номер 10
-// ... у всех стрелков будет номер 10, вместо 0, 1, 2, 3...
+let arr = [1, 2, 3, 4, 5, 6, 7];
+alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
