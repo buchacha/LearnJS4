f3d4571da9ae0520cc3033f9ee7cc7528d0364ae
buchacha <egor322129@gmail.com>
2020-09-15 07:52:15 +0000

    array equal using

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let arr1 = [1, -2, 3, 4];
-let arr2 = [8, 3, -8, 1];
+let arr1 = [1, {john:12}, 3, 4];
 
-alert( Math.max(1, ...arr1, 2, ...arr2, 25) ); // 25
+
+let arr2 = [...arr1];
+
+arr3 = arr1;
+
+alert(arr1 === arr2); // false
+
+alert(arr1 === arr3); // true
+
+
+arr2[1].john = 34; 
+
+alert(arr1[1].john); // 34
