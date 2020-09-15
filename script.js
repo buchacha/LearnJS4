2a7e248c431c21171223b44ab1ccc3e3af7bf222
buchacha <egor322129@gmail.com>
2020-09-15 10:50:49 +0000

    counter with closure and methods

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-let sayHi = function func(who) {
-  if (who) {
-    alert(`Hello, ${who}`);
-  } else {
-    func("Guest"); // Теперь всё в порядке
-  }
-};
+function makeCounter() {
 
-let welcome = sayHi;
-sayHi = null;
+  let count = 0;
 
-welcome(); // Hello, Guest (вложенный вызов работает)
+  function counter() {
+    return count++;
+  };
+
+  counter.set = (value) => count = value;
+
+  counter.decrease = () => count--;
+
+  return counter;
+}
+
+
+let counter = makeCounter();
+alert( counter() ); // 0
+alert( counter() ); // 1
+
+
+counter.set(10);
+alert( counter() ); // 10
+
+counter.decrease();
+counter.decrease();
+
+alert( counter() );
