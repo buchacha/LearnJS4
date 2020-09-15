6096ad1b6148890119864309c3caafa207d0e5ff
buchacha <egor322129@gmail.com>
2020-09-15 12:06:53 +0000

    setTimeout

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-function sum(a) {
+let timerId = setTimeout(() => alert("ничего не происходит"), 1000);
+alert(timerId); // идентификатор таймера
 
-  let currentSum = a;
-
-  function f(b) {
-    currentSum += b;
-    return f;
-  }
-
-  f.toString = function() {
-    return currentSum;
-  };
-
-  return f;
-}
-
-sum(1)(2) == 3; // 1 + 2
-sum(1)(2)(3) == 6; // 1 + 2 + 3
-sum(5)(-1)(2) == 6
-sum(6)(-1)(-2)(-3) == 0
-sum(0)(1)(2)(3)(4)(5) == 15
+clearTimeout(timerId);
+alert(timerId); // тот же идентификатор (не принимает значение null после отмены)
